6428a7c64d71a5399dd3cbebfa03389a5688cff8
GRUDE <kristoffenbg@gmail.com>
2021-07-04 11:25:56 +0200

    add styled components and eslint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div>
-        <p>SUMMERPROJECT </p>
-        <p>Hei folkens! Dette er nettiden vår som vi kan gøre de endringene vi vil på.</p>
-        <p>Jeg har nå fikset det viktigste oppsettet så alt skal være klart til å progge i vei.</p>
-        <p>Dette blir bra!!! Bare spørr om det er noe:)</p>
-        <p>Hilsen Grude</p>
-        <button>Hello</button>
+      <p>SUMMERPROJECT </p>
+      <p>Hei folkens! Dette er nettiden vår som vi kan gøre de endringene vi vil på.</p>
+      <p>Jeg har nå fikset det viktigste oppsettet så alt skal være klart til å progge i vei.</p>
+      <p>Dette blir bra!!! Bare spørr om det er noe:)</p>
+      <p>Hilsen Grude</p>
+      <button>Hello</button>
     </div>
   );
 }
